71c54c8d30652ae823f71397da7888ad2f613d77
Francisco Maldonado <fmald002@sledge.cs.ucr.edu>
2021-11-21 19:17:28 -0800

    Lab3 test file from TA

--- lab3.c
+++ lab3.c
@@ -1,21 +1,38 @@
 #include "types.h"
 #include "user.h"
 
-#pragma GCC optimize ("O0") //This stops tail end optimization to make sure we need more pages to be created
-int recursive (int n) {
-    if (n == 0) {
-        return n;
-    } else {
-        //printf(1, "something");
-        return 1 + recursive(n - 1);
-    }
-}
-
-int main(int argc, char *argv[])
+// Prevent this function from being optimized, which might give it closed form
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
+static int
+recurse(int n)
 {
-    int n = atoi(argv[1]);
-    int result = recursive(n);
-    printf(2, "Result: %d", result);
-    exit();
+  if(n == 0)
     return 0;
+  return n + recurse(n - 1);
+}
+#pragma GCC pop_options
+
+int
+main(int argc, char *argv[])
+{
+  int v, n, m;
+
+  if (argc == 1) {
+    v = argc;
+    printf(1, "Lab 3 (part 1) - Address at first location: %p\n", &v);
+    exit();
+  }
+  else if (argc == 2) {
+    n = atoi(argv[1]);
+    printf(1, "Lab 3 (part 2) - Recursing %d levels\n", n);
+    m = recurse(n);
+    printf(1, "Lab 3 (part 2) - Yielded a value of %d\n", m);
+    exit();
+  }
+  else {
+    printf(1, "Test Part 1 - Usage: $ %s\n", argv[0]);
+    printf(1, "Test Part 2 - Usage: $ %s <levels>\n", argv[0]);
+    exit();
+  }
 }
